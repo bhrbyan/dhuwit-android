78da43be5eddf9b6769be61225cb2582da194218
bhrbyan <by.devlpr@gmail.com>
2022-05-21 22:02:12 +0700

    Update design account and primary account

--- Dhuwit/features/feature-account/src/main/java/id/dhuwit/feature/account/ui/main/adapter/AccountMainViewHolder.kt
+++ Dhuwit/features/feature-account/src/main/java/id/dhuwit/feature/account/ui/main/adapter/AccountMainViewHolder.kt
@@ -1,10 +1,10 @@
 package id.dhuwit.feature.account.ui.main.adapter
 
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import id.dhuwit.core.account.model.Account
 import id.dhuwit.core.extension.convertPriceWithCurrencyFormat
-import id.dhuwit.core.extension.gone
-import id.dhuwit.core.extension.visible
+import id.dhuwit.feature.account.R
 import id.dhuwit.feature.account.databinding.AccountMainItemBinding
 import id.dhuwit.storage.Storage
 
@@ -12,14 +12,23 @@ class AccountMainViewHolder(private val binding: AccountMainItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
     fun onBind(account: Account, storage: Storage) {
-        binding.textAccountName.text = account.name
+        binding.textAccountName.text =
+            binding.root.context.getString(R.string.account_main_account_name, account.name)
         binding.textAccountBalance.text =
             account.balance.convertPriceWithCurrencyFormat(storage.getSymbolCurrency())
 
         if (account.isPrimary) {
-            binding.imagePrimaryAccount.visible()
+            binding.cardAccount.strokeWidth = 8
+            binding.cardAccount.strokeColor = ContextCompat.getColor(
+                binding.root.context,
+                R.color.colorPrimaryAccount
+            )
         } else {
-            binding.imagePrimaryAccount.gone()
+            binding.cardAccount.strokeWidth = 0
+            binding.cardAccount.strokeColor = ContextCompat.getColor(
+                binding.root.context,
+                R.color.white
+            )
         }
     }
 
