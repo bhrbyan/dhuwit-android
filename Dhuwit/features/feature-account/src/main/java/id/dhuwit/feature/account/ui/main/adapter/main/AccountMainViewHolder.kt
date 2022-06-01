dee286f2546817c56db8fe0fdcdf17932d3bb989
bhrbyan <by.devlpr@gmail.com>
2022-06-01 22:05:49 +0700

    Change design primary account

--- Dhuwit/features/feature-account/src/main/java/id/dhuwit/feature/account/ui/main/adapter/main/AccountMainViewHolder.kt
+++ Dhuwit/features/feature-account/src/main/java/id/dhuwit/feature/account/ui/main/adapter/main/AccountMainViewHolder.kt
@@ -1,9 +1,10 @@
 package id.dhuwit.feature.account.ui.main.adapter.main
 
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import id.dhuwit.core.account.model.Account
 import id.dhuwit.core.extension.convertPriceWithCurrencyFormat
+import id.dhuwit.core.extension.gone
+import id.dhuwit.core.extension.visible
 import id.dhuwit.feature.account.R
 import id.dhuwit.feature.account.databinding.AccountMainItemBinding
 import id.dhuwit.storage.Storage
@@ -18,17 +19,9 @@ class AccountMainViewHolder(private val binding: AccountMainItemBinding) :
             account.balance.convertPriceWithCurrencyFormat(storage.getSymbolCurrency())
 
         if (account.isPrimary) {
-            binding.cardAccount.strokeWidth = 8
-            binding.cardAccount.strokeColor = ContextCompat.getColor(
-                binding.root.context,
-                R.color.colorPrimaryAccount
-            )
+            binding.imagePrimaryAccount?.visible()
         } else {
-            binding.cardAccount.strokeWidth = 0
-            binding.cardAccount.strokeColor = ContextCompat.getColor(
-                binding.root.context,
-                R.color.white
-            )
+            binding.imagePrimaryAccount?.gone()
         }
     }
 
